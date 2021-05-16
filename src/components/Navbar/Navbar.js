46289973eaef468be7e045ae9e423d9073be6fc8
Donna <skies09@hotmail.com>
2021-05-16 14:48:38 +0100

    sort nav out and add background url

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 import { SidebarData } from './SidebarData';
 import './Navbar.css';
 import { IconContext } from 'react-icons';
-import Social from '../components/Social';
+import Social from '../../components/Social';
 
 function Navbar() {
   const [sidebar, setSidebar] = useState(false);
